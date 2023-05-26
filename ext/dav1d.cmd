4c6d4509f7d2336881f7b5112fcb3196336755aa
cosmonaut <evan@moonside.games>
2023-05-26 16:16:16 -0700

    initial commit

---
+++ ext/dav1d.cmd
@@ -0,0 +1,19 @@
+: # Run this before configuring CMake.
+
+: # meson and ninja must be in your PATH.
+
+: # NOTE: on Windows you must run this through the Visual Studio command prompt
+: # NOTE: on Windows x86 you must install NASM 2.14 or higher: https://nasm.us/
+
+git clone -b 1.2.0 --depth 1 https://code.videolan.org/videolan/dav1d.git
+
+cd dav1d
+mkdir build
+cd build
+
+: # macOS might require: -Dc_args=-fno-stack-check
+: # Build with asan: -Db_sanitize=address
+: # Build with ubsan: -Db_sanitize=undefined
+meson setup --default-library=static --buildtype release ..
+ninja
+cd ../..
