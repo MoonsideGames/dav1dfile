0400958055b1dd30d8c3f8223bf6c07ed8174fb7
cosmonaut <evan@moonside.games>
2023-05-31 10:47:35 -0700

    add interop csproj

---
+++ csharp/dav1dfile.cs
@@ -0,0 +1,75 @@
+/* dav1dfile# - C# Wrapper for dav1dfile
+ *
+ * Copyright (c) 2023 Evan Hemsley
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from
+ * the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software in a
+ * product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ * Evan "cosmonaut" Hemsley <evan@moonside.games>
+ *
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+public static class dav1dfile
+{
+	const string nativeLibName = "dav1dfile";
+
+	public const uint DAV1DFILE_MAJOR_VERSION = 0;
+	public const uint DAV1DFILE_MINOR_VERSION = 1;
+	public const uint DAV1DFILE_PATCH_VERSION = 0;
+
+	public const uint DAV1DFILE_COMPILED_VERSION = (
+		(DAV1DFILE_MAJOR_VERSION * 100 * 100) +
+		(DAV1DFILE_MINOR_VERSION * 100) +
+		(DAV1DFILE_PATCH_VERSION)
+	);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	public extern int df_fopen(
+		[MarshalAs(UnmanagedType.LPUTF8Str)] string filename,
+		out IntPtr context
+	);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	public extern void df_close(IntPtr context);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	public extern void df_videoinfo(
+		IntPtr context,
+		out int width,
+		out int height,
+		out double fps
+	);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	public extern int df_eos(IntPtr context);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	public extern void df_reset(IntPtr context);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	public extern int df_readvideo(
+		IntPtr context,
+		int numFrames,
+		out IntPtr yDataPtr,
+		out IntPtr uDataPtr,
+		out IntPtr vDataPtr
+	);
+}
