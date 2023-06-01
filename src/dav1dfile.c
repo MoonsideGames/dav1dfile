0648f8a6ba58cfb59e0c000550ecd12c310e994b
cosmonaut <evan@moonside.games>
2023-05-31 17:23:54 -0700

    more decoder structure

--- src/dav1dfile.c
+++ src/dav1dfile.c
@@ -51,6 +51,125 @@ typedef struct Context {
 	uint8_t eof;
 } Context;
 
+static void allocator_no_op(const uint8_t *data, void *opaque)
+{
+	/* no-op */
+}
+
+static inline uint32_t aligned(
+	uint32_t n,
+	uint32_t alignment
+) {
+	return ((n + (alignment - 1)) & ~(alignment - 1));
+}
+
+/* Aligned alloc functions borrowed from SDL 3.0 */
+
+# define SDL_SIZE_MAX ((size_t) -1)
+
+int size_add_overflow (size_t a, size_t b, size_t *ret)
+{
+    if (b > SIZE_MAX - a) {
+        return -1;
+    }
+    *ret = a + b;
+    return 0;
+}
+
+void *aligned_alloc(size_t alignment, size_t size)
+{
+    size_t padding;
+    uint8_t *retval = NULL;
+
+    if (alignment < sizeof(void*)) {
+        alignment = sizeof(void*);
+    }
+    padding = (alignment - (size % alignment));
+
+    if (size_add_overflow(size, alignment, &size) == 0 &&
+        size_add_overflow(size, sizeof(void *), &size) == 0 &&
+        size_add_overflow(size, padding, &size) == 0) {
+        void *original = malloc(size);
+        if (original) {
+            /* Make sure we have enough space to store the original pointer */
+            retval = (uint8_t *)original + sizeof(original);
+
+            /* Align the pointer we're going to return */
+            retval += alignment - (((size_t)retval) % alignment);
+
+            /* Store the original pointer right before the returned value */
+            memcpy(retval - sizeof(original), &original, sizeof(original));
+        }
+    }
+    return retval;
+}
+
+void aligned_free(void *mem)
+{
+    if (mem) {
+        void *original;
+        memcpy(&original, ((uint8_t *)mem - sizeof(original)), sizeof(original));
+        free(original);
+    }
+}
+
+int picture_alloc_no_op(Dav1dPicture *picture, void *opaque)
+{
+	uint32_t yLength;
+	uint32_t uvLength;
+	uint32_t chromaWidth;
+
+	yLength = aligned(picture->p.w, 128) * aligned(picture->p.h, 128);
+
+	if (picture->p.layout == DAV1D_PIXEL_LAYOUT_I420)
+	{
+		chromaWidth = picture->p.w / 2;
+		uvLength =
+			aligned(chromaWidth, 128) *
+			aligned(picture->p.h / 2, 128);
+	}
+	else if (picture->p.layout == DAV1D_PIXEL_LAYOUT_I422)
+	{
+		chromaWidth = picture->p.w / 2;
+		uvLength =
+			aligned(chromaWidth, 128) *
+			aligned(picture->p.h, 128);
+	}
+	else if (picture->p.layout == DAV1D_PIXEL_LAYOUT_I444)
+	{
+		chromaWidth = picture->p.w;
+		uvLength =
+			aligned(picture->p.w, 128) *
+			aligned(picture->p.h, 128);
+	}
+	else
+	{
+		/* I400 format not supported!*/
+		return -1;
+	}
+
+	yLength = aligned(yLength, DAV1D_PICTURE_ALIGNMENT);
+	uvLength = aligned(uvLength, DAV1D_PICTURE_ALIGNMENT);
+
+	picture->allocator_data = aligned_alloc(DAV1D_PICTURE_ALIGNMENT, yLength + uvLength + uvLength);
+	picture->data[0] = picture->allocator_data;
+	picture->data[1] = (uint8_t*) picture->allocator_data + yLength;
+	picture->data[2] = (uint8_t*) picture->allocator_data + yLength + uvLength;
+
+	memset(picture->allocator_data, '\0', aligned(yLength + uvLength + uvLength, DAV1D_PICTURE_ALIGNMENT));
+
+	/* FIXME: are these stride values correct */
+	picture->stride[0] = aligned(picture->p.w, 128);
+	picture->stride[1] = aligned(chromaWidth, 128);
+
+	return 0;
+}
+
+static void picture_free_no_op(Dav1dPicture *picture, void *opaque)
+{
+	aligned_free(picture->allocator_data);
+}
+
 static inline int INTERNAL_getNextPacket(
 	Context *context
 ) {
@@ -61,6 +180,8 @@ static inline int INTERNAL_getNextPacket(
 	OBPError error;
 	uint8_t* bitstreamPtr;
 	int result;
+
+	error.size = 0;
 
 	context->bitstreamIndex += context->currentOBUSize;
 	bitstreamPtr = context->bitstreamData + context->bitstreamIndex;
@@ -98,15 +219,19 @@ static int df_open_from_memory(uint8_t *bytes, uint32_t size, AV1_Context **cont
 	internalContext->eof = 0;
 	internalContext->width = 0;
 	internalContext->height = 0;
+	memset(&internalContext->currentPicture, '\0', sizeof(Dav1dPicture));
 
 	dav1d_default_settings(&settings);
+	settings.allocator.alloc_picture_callback = picture_alloc_no_op;
+	settings.allocator.release_picture_callback = picture_free_no_op;
+	settings.allocator.cookie = internalContext;
 
 	result = dav1d_open(&dav1dContext, &settings);
 	if (result < 0)
 	{
 		free(dav1dContext);
 		free(internalContext);
-		return -1;
+		return 0;
 	}
 
 	internalContext->dav1dContext = dav1dContext;
@@ -136,12 +261,16 @@ static int df_open_from_memory(uint8_t *bytes, uint32_t size, AV1_Context **cont
 	{
 		free(dav1dContext);
 		free(internalContext);
-		return -1;
+		return 0;
 	}
+
+	/* Reset the stream index */
+	internalContext->bitstreamIndex = 0;
+	internalContext->currentOBUSize = 0;
 
 	*context = (AV1_Context*) internalContext;
 
-	return 0;
+	return 1;
 }
 
 static int df_open_from_file(FILE *file, AV1_Context **context)
@@ -168,7 +297,7 @@ int df_fopen(const char *fname, AV1_Context **context)
 		return df_open_from_file(f, context);
 	}
 
-	return -1;
+	return 0;
 }
 
 void df_videoinfo(
@@ -186,6 +315,32 @@ void df_videoinfo(
 	*pixelLayout = internalContext->pixelLayout;
 }
 
+static int read_data(Context *internalContext, Dav1dData *data)
+{
+	if (internalContext->eof)
+	{
+		return 0;
+	}
+
+	if (!INTERNAL_getNextPacket(internalContext))
+	{
+		internalContext->eof = 1;
+		return 0;
+	}
+
+	if (dav1d_data_wrap(data, internalContext->bitstreamData + internalContext->bitstreamIndex, internalContext->currentOBUSize, allocator_no_op, NULL) < 0)
+	{
+		return 0;
+	}
+
+	return 1;
+}
+
+static void unref_picture(Context *internalContext)
+{
+	dav1d_picture_unref(&internalContext->currentPicture);
+}
+
 int df_readvideo(AV1_Context *context,int numFrames, void **yData, void **uData, void **vData)
 {
 	Context *internalContext = (Context*) context;
@@ -196,40 +351,34 @@ int df_readvideo(AV1_Context *context,int numFrames, void **yData, void **uData,
 
 	for (i = 0; i < numFrames; i += 1)
 	{
-		/* Get a packet... */
-		if (!INTERNAL_getNextPacket(internalContext))
-		{
-			/* ... unless there's nothing left for us to read. */
-			internalContext->eof = 1;
-			break;
-		}
+		unref_picture(internalContext);
+		read_data(internalContext, &data);
 
-		/* Let's send some data to the decoder... */
-		data.data = internalContext->bitstreamData + internalContext->bitstreamIndex;
-		data.sz = internalContext->currentOBUSize;
-		data.ref = NULL;
-
-		sendDataResult = dav1d_send_data(internalContext->dav1dContext, &data);
-
-		while (sendDataResult == DAV1D_ERR(EAGAIN))
+		do
 		{
 			sendDataResult = dav1d_send_data(internalContext->dav1dContext, &data);
-		}
 
-		if (sendDataResult != 0)
-		{
-			/* Something went wrong on data send! */
-			return -1;
-		}
+			if (sendDataResult < 0 && sendDataResult != DAV1D_ERR(EAGAIN))
+			{
+				/* Something went wrong on data send! */
+				return 0;
+			}
 
-		/* Time to get a picture... */
-		getPictureResult = dav1d_get_picture(internalContext->dav1dContext, &internalContext->currentPicture);
+			getPictureResult = dav1d_get_picture(internalContext->dav1dContext, &internalContext->currentPicture);
 
-		if (getPictureResult != 0)
-		{
-			/* Something went wrong on picture get! */
-			return -1;
-		}
+			if (getPictureResult < 0)
+			{
+				if (getPictureResult != DAV1D_ERR(EAGAIN))
+				{
+					/* Something went wrong on picture get! */
+					return 0;
+				}
+			}
+			else
+			{
+				break;
+			}
+		} while (data.sz || read_data(internalContext, &data) == 1);
 	}
 
 	/* Set the picture data pointers */
@@ -237,7 +386,7 @@ int df_readvideo(AV1_Context *context,int numFrames, void **yData, void **uData,
 	*uData = internalContext->currentPicture.data[1];
 	*vData = internalContext->currentPicture.data[2];
 
-	return 0;
+	return 1;
 }
 
 int df_eos(AV1_Context *context)
