8eefd7d7381fbcdc5939186df61d6340c454942b
Evan Hemsley <2342303+thatcosmonaut@users.noreply.github.com>
2024-10-21 15:23:19 -0700

    CI build changes (#1)

--- ext/dav1d_osx.sh
+++ ext/dav1d_osx.sh
@@ -1,21 +1,25 @@
 : # Run this before configuring CMake.
-
 : # meson and ninja must be in your PATH.
-
-: # NOTE: on Windows you must run this through the Visual Studio command prompt
-: # NOTE: on Windows x86 you must install NASM 2.14 or higher: https://nasm.us/
 
 git clone -b 1.4.0 --depth 1 https://code.videolan.org/videolan/dav1d.git
 
 cd dav1d
+mkdir -p arm64
+cd arm64
+
+MACOSX_DEPLOYMENT_TARGET=11.0 meson setup --default-library=static --buildtype release ..
+MACOSX_DEPLOYMENT_TARGET=11.0 ninja
+
+cd ..
+mkdir -p x64
+cd x64
+
+MACOSX_DEPLOYMENT_TARGET=11.0 meson setup --cross-file=../../ext/macos_x64.txt --default-library=static --buildtype release ..
+MACOSX_DEPLOYMENT_TARGET=11.0 ninja
+
+cd ..
 mkdir -p build
-cd build
-
-: # macOS might require: -Dc_args=-fno-stack-check
-: # Build with asan: -Db_sanitize=address
-: # Build with ubsan: -Db_sanitize=undefined
-
-MACOSX_DEPLOYMENT_TARGET=10.9 meson setup --default-library=static --buildtype release ..
-MACOSX_DEPLOYMENT_TARGET=10.9 ninja
-
-cd ../..
+mkdir -p build/src
+lipo -create -output build/src/libdav1d.a arm64/src/libdav1d.a x64/src/libdav1d.a
+cp -r arm64/include build/include
+cd ..
