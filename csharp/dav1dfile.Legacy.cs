04377871ff397239a857dcda5e22a0cb845119d9
kiddkaffeine <miles@Ms-MacBook-Pro.local>
2026-01-09 00:47:43 -0500

    Check namespace usage in Legacy C# file for older net compatibility

--- csharp/dav1dfile.Legacy.cs
+++ csharp/dav1dfile.Legacy.cs
@@ -28,124 +28,127 @@ using System;
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace Dav1dfile;
-
-public static class Bindings
+namespace Dav1dfile
 {
-	const string nativeLibName = "dav1dfile";
-
-	public const uint DAV1DFILE_MAJOR_VERSION = 1;
-	public const uint DAV1DFILE_MINOR_VERSION = 0;
-	public const uint DAV1DFILE_PATCH_VERSION = 0;
-
-	public const uint DAV1DFILE_COMPILED_VERSION = (
-		(DAV1DFILE_MAJOR_VERSION * 100 * 100) +
-		(DAV1DFILE_MINOR_VERSION * 100) +
-		(DAV1DFILE_PATCH_VERSION)
-	);
-
-	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-	public static extern uint df_linked_version();
-
-	public enum PixelLayout
+	public static class Bindings
 	{
-		I400,
-		I420,
-		I422,
-		I444
-	}
+		const string nativeLibName = "dav1dfile";
 
-	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-	public extern static int df_open_from_memory(
-		IntPtr bytes,
-		uint size,
-		out IntPtr context
-	);
+		public const uint DAV1DFILE_MAJOR_VERSION = 1;
+		public const uint DAV1DFILE_MINOR_VERSION = 0;
+		public const uint DAV1DFILE_PATCH_VERSION = 0;
 
-	[DllImport(nativeLibName, EntryPoint = "df_fopen", CallingConvention = CallingConvention.Cdecl)]
-	private static extern unsafe int INTERNAL_df_fopen(
-		[MarshalAs(UnmanagedType.LPStr)] string fname,
-		out IntPtr context
-	);
+		public const uint DAV1DFILE_COMPILED_VERSION = (
+			(DAV1DFILE_MAJOR_VERSION * 100 * 100) +
+			(DAV1DFILE_MINOR_VERSION * 100) +
+			(DAV1DFILE_PATCH_VERSION)
+		);
 
-	[DllImport(nativeLibName, EntryPoint = "df_fopen", CallingConvention = CallingConvention.Cdecl)]
-	private static extern unsafe int INTERNAL_df_fopen(
-		byte* fname,
-		out IntPtr context
-	);
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern uint df_linked_version();
 
-	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-	public extern static void df_close(IntPtr context);
-
-	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-	public extern static void df_videoinfo(
-		IntPtr context,
-		out int width,
-		out int height,
-		out PixelLayout pixelLayout
-	);
-
-	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-	public extern static void df_videoinfo2(
-		IntPtr context,
-		out int width,
-		out int height,
-		out PixelLayout pixelLayout,
-		out byte hbd
-	);
-
-	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-	public extern static int df_eos(IntPtr context);
-
-	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-	public extern static void df_reset(IntPtr context);
-
-	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-	public extern static int df_readvideo(
-		IntPtr context,
-		int numFrames,
-		out IntPtr yDataPtr,
-		out IntPtr uDataPtr,
-		out IntPtr vDataPtr,
-		out uint yDataLength,
-		out uint uvDataLength,
-		out uint yStride,
-		out uint uvStride
-	);
-
-	/* Used for heap allocated string marshaling
-	 * Returned byte* must be free'd with FreeHGlobal.
-	 */
-	private static unsafe byte* Utf8Encode(string str)
-	{
-		int bufferSize = (str.Length * 4) + 1;
-		byte* buffer = (byte*) Marshal.AllocHGlobal(bufferSize);
-		fixed (char* strPtr = str)
+		public enum PixelLayout
 		{
-			Encoding.UTF8.GetBytes(
-				strPtr,
-				str.Length + 1,
-				buffer,
-				bufferSize
-			);
+			I400,
+			I420,
+			I422,
+			I444
 		}
-		return buffer;
-	}
 
-	public static unsafe int df_fopen(string fname, out IntPtr context)
-	{
-		int result;
-		if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public extern static int df_open_from_memory(
+			IntPtr bytes,
+			uint size,
+			out IntPtr context
+		);
+
+		[DllImport(nativeLibName, EntryPoint = "df_fopen", CallingConvention = CallingConvention.Cdecl)]
+		private static extern unsafe int INTERNAL_df_fopen(
+			[MarshalAs(UnmanagedType.LPStr)] string fname,
+			out IntPtr context
+		);
+
+		[DllImport(nativeLibName, EntryPoint = "df_fopen", CallingConvention = CallingConvention.Cdecl)]
+		private static extern unsafe int INTERNAL_df_fopen(
+			byte* fname,
+			out IntPtr context
+		);
+
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public extern static void df_close(IntPtr context);
+
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public extern static void df_videoinfo(
+			IntPtr context,
+			out int width,
+			out int height,
+			out PixelLayout pixelLayout
+		);
+
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public extern static void df_videoinfo2(
+			IntPtr context,
+			out int width,
+			out int height,
+			out PixelLayout pixelLayout,
+			out byte hbd
+		);
+
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public extern static int df_eos(IntPtr context);
+
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public extern static void df_reset(IntPtr context);
+
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public extern static int df_readvideo(
+			IntPtr context,
+			int numFrames,
+			out IntPtr yDataPtr,
+			out IntPtr uDataPtr,
+			out IntPtr vDataPtr,
+			out uint yDataLength,
+			out uint uvDataLength,
+			out uint yStride,
+			out uint uvStride
+		);
+
+		/* Used for heap allocated string marshaling
+		 * Returned byte* must be free'd with FreeHGlobal.
+		 */
+		private static unsafe byte* Utf8Encode(string str)
 		{
-			/* Windows fopen doesn't like UTF8, use LPCSTR and pray */
-			result = INTERNAL_df_fopen(fname, out context);
+			int bufferSize = (str.Length * 4) + 1;
+			byte* buffer = (byte*) Marshal.AllocHGlobal(bufferSize);
+			fixed (char* strPtr = str)
+			{
+				Encoding.UTF8.GetBytes(
+					strPtr,
+					str.Length + 1,
+					buffer,
+					bufferSize
+				);
+			}
+
+			return buffer;
 		}
-		else
+
+		public static unsafe int df_fopen(string fname, out IntPtr context)
 		{
-			byte* utf8Fname = Utf8Encode(fname);
-			result = INTERNAL_df_fopen(utf8Fname, out context);
-			Marshal.FreeHGlobal((IntPtr) utf8Fname);
+			int result;
+			if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+			{
+				/* Windows fopen doesn't like UTF8, use LPCSTR and pray */
+				result = INTERNAL_df_fopen(fname, out context);
+			}
+			else
+			{
+				byte* utf8Fname = Utf8Encode(fname);
+				result = INTERNAL_df_fopen(utf8Fname, out context);
+				Marshal.FreeHGlobal((IntPtr) utf8Fname);
+			}
+
+			return result;
 		}
-		return result;
 	}
 }
